af756c3cf17c9cb7ac94fde02b1f99bf052cc92d
han44-illinois <74620710+han44-illinois@users.noreply.github.com>
2025-08-20 12:35:57 -0500

    Update Invoke-CCMCacheCleanup.ps1
    Added a little bit of reporting to the terminal for this command

--- Functions/Invoke-CCMCacheCleanup.ps1
+++ Functions/Invoke-CCMCacheCleanup.ps1
@@ -5,12 +5,16 @@ function Invoke-CCMCacheCleanup {
 
     if(Test-Connection $Computer -Count 1 -Quiet){
         Write-Host "$Computer successfully pinged, clearing its ccmcache."
+        Push-Location -Path "C:\engrit"
+        $NumFolders = Get-ChildItem -Path "\\$Computer\c`$\Windows\ccmcache"
+        Write-Host "Removing $NumFolders folders from $Computer's ccmcache."
         Invoke-Command -ComputerName $Computer -ScriptBlock {
             [__comobject]$CCMComObject = New-Object -ComObject 'UIResource.UIResourceMgr'
             $CacheInfo = $CCMComObject.GetCacheInfo().GetCacheElements()
             foreach ($CacheItem in $CacheInfo) {
             $null = $CCMComObject.GetCacheInfo().DeleteCacheElement([string]$($CacheItem.CacheElementID))}
         }
+        Pop-Location
     }
     
 }
