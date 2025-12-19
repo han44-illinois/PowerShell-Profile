890e0d2523ad7090eb56fb53a3297a60efe95281
han44-illinois <74620710+han44-illinois@users.noreply.github.com>
2025-12-19 16:02:32 -0600

    Some more fixes

--- Functions/Invoke-CMCollectionUpdateForInstantImaging.ps1
+++ Functions/Invoke-CMCollectionUpdateForInstantImaging.ps1
@@ -11,17 +11,17 @@ function Invoke-CMCollectionUpdateForInstantImaging {
     }
 
     PROCESS {
-        Invoke-CMDeviceCollectionUpdate -Name "UIUC-ENGR-IS OSD Available TS + No Maintenance Window"
+        Invoke-CMDeviceCollectionUpdate -Name "UIUC-ENGR-IS Deploy OSD Available TS + No Maintenance Window"
         Invoke-CMDeviceCollectionUpdate -Name "UIUC-ENGR-IS Deploy OSD TS (Win11 2025a, Available, with SC)"
         Invoke-CMDeviceCollectionUpdate -Name "UIUC-ENGR-IS Maint Window - Exclude from ALL windows"
         Invoke-CMDeviceCollectionUpdate -Name "UIUC-ENGR-IS Maint Window - Exclude from Standard window"
-        Invoke-CMDeviceCollectionUpdate -Name "UIUC-ENGR-IS Maint Window - Machines not in ANY maint window collection (Alternate)"
+        Invoke-CMDeviceCollectionUpdate -Name "UIUC-ENGR-IS Maint Window - Machines not in ANY maint window collection"
         Invoke-CMDeviceCollectionUpdate -Name "UIUC-ENGR-IS Maint Window - Standard window"
         Write-Host "$(Get-Date -DisplayHint Time) Collection updates initiated! Try Invoke-TaskSequence in like 15 minutes."
         Write-Host "$(Get-Date -DisplayHint Time) Waiting 15 minutes to push policy..."
 
         Start-Sleep -Seconds 900
-        Invoke-CMClientAction -CollectionName "UIUC-ENGR-IS OSD Available TS + No Maintenance Window" -ActionType ClientNotificationRequestMachinePolicyNow
+        Invoke-CMClientAction -CollectionName "UIUC-ENGR-IS Deploy OSD Available TS + No Maintenance Window" -ActionType ClientNotificationRequestMachinePolicyNow
 
         $DeploymentID = (Get-CMDeployment -CollectionName "UIUC-ENGR-IS OSD TS (Win11 2023c, Available, with SC)").DeploymentID
         Write-Host "Reminder: When you're ready, invoke task sequence with deployment id $DeploymentID"
