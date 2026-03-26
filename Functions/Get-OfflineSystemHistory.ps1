2fb0c013fb4e99619d50aedc36163bfc8a37dcfc
han44-illinois <74620710+han44-illinois@users.noreply.github.com>
2026-03-26 12:16:19 -0500

    feat: added the ability to copy the latest results
    Added parameters `$Current` and `$OpenSheet`.
    
    When `$Current` is specified, the function will copy the contents of the `offline-systems_latest.tsv` to the clipboard.
    
    When `$OpenSheet` is specified, the function will also open the offline google sheet in the default browser.

--- Functions/Get-OfflineSystemHistory.ps1
+++ Functions/Get-OfflineSystemHistory.ps1
@@ -1,24 +1,45 @@
 function Get-OfflineSystemHistory {
 param(
-    [Parameter(Mandatory)]
+    [Parameter(Mandatory, ParameterSetName = 'Current')]
+    [switch]$Current,
+
+    [Parameter(ParameterSetName = 'Current')]
+    [switch]$OpenSheet,
+
+    [Parameter(Mandatory, ParameterSetName = 'History')]
     [string]$Year,
-    [Parameter(Mandatory)]
+
+    [Parameter(Mandatory, ParameterSetName = 'History')]
     [string]$Month,
+
+    [Parameter(ParameterSetName = 'History')]
     [ValidateSet('Windows','Linux')]
     [string]$OS
     )
 
-    $Output = Get-ChildItem -Path "\\engr-is-auto01\share\automations\data\get-offline-systems-output\offline-systems_$Year-$Month*.csv" | 
-        ForEach-Object { Get-Content -Path $_ | ConvertFrom-Csv -Header "name","os" }
-        
-    if($OS){
-        $Output = $Output | Where-Object {$_.os -eq $OS}
-    }
+    switch ($PSCmdlet.ParameterSetName) {
+        'Current' {
+            $Output = Get-Content -Path "\\engr-is-auto01\share\automations\data\get-offline-systems-output\offline-systems_latest.tsv"
+            $Output | Set-Clipboard
+            Write-Host "Latest offline system sheet has been copied to your clipboard!"
+            if($OpenSheet){
+                Start-Process "https://docs.google.com/spreadsheets/d/1-LmMafGDrZ8bKzfWLA2UbbdQd2S92_Smi1yT-eetWNk/edit?gid=0#gid=0"
+            }
+        }
+        'History' {
+            $Output = Get-ChildItem -Path "\\engr-is-auto01\share\automations\data\get-offline-systems-output\offline-systems_$Year-$Month*.csv" | 
+                ForEach-Object { Get-Content -Path $_ | ConvertFrom-Csv -Header "name","os" }
+            
+            if($OS){
+                $Output = $Output | Where-Object {$_.os -eq $OS}
+            }
 
-    $Output = $Output | Select-Object -ExpandProperty name | 
-    Group-Object | 
-    Select-Object -Property Count,Name | 
-    Sort-Object -Property Count -Descending
+            $Output = $Output | Select-Object -ExpandProperty name | 
+            Group-Object | 
+            Select-Object -Property Count,Name | 
+            Sort-Object -Property Count -Descending
+        }
+    }
 
     $Output
 }
