4efbc3a2cff938acb2816bd270dfa5f299247a27
han44-illinois <74620710+han44-illinois@users.noreply.github.com>
2026-06-08 16:48:00 -0500

    Create Get-BIOSFromLab.ps1

---
+++ Functions/Get-BIOSFromLab.ps1
@@ -0,0 +1,60 @@
+function Get-BIOSFromLab{
+
+    <#
+    .SYNOPSIS
+        Queries the Win32_BIOS for an entire computer lab.
+
+    .DESCRIPTION
+        A convenience function to query the Win32_BIOS for an entire computer lab.
+
+    .PARAMETER comps
+        Computer Lab name with wildcard e.g. eh-110a-*
+
+    .EXAMPLE
+        PS>
+
+        Example of how to use this cmdlet
+
+    .EXAMPLE
+        PS>
+
+        Another example of how to use this cmdlet
+
+    .INPUTS
+        Inputs to this cmdlet (if any)
+
+    .OUTPUTS
+        Output from this cmdlet (if any)
+
+    .LINK
+        Any related function or website
+
+    .NOTES
+        General notes
+    #>
+
+    [CmdletBinding()]
+
+    param(
+        [Parameter()]
+        [string] $comps
+    )
+    
+
+    BEGIN{}
+
+    PROCESS{
+        Get-ADComputer -Filter {name -like $comps} -SearchBase "OU=Instructional,OU=Desktops,OU=Engineering,OU=Urbana,DC=ad,DC=uillinois,DC=edu" | 
+            ForEach-Object -ThrottleLimit 20 -Parallel {
+                if(Test-Connection $_.Name -IPv4 -Quiet -Count 1){ 
+                    Get-CimInstance -ClassName Win32_BIOS -ComputerName $_.Name | Select-Object PSComputerName,SMBIOSBIOSVersion 
+                }else{
+                    Write-Host "$($_.Name) did not respond" 
+                }
+            } | 
+                Sort-Object -Property PSComputerName
+        
+    }
+
+    END{}
+}
